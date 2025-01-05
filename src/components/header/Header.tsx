d9defc24c685d8cead054ae3830cd37d84b8e854
Deepak Meena <deepak07itmeena@gmail.com>
2025-01-05 10:30:40 +0530

    3rd commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,30 @@
 import React from "react";
 import logo from "../../images/logo.png";
 import Image from "next/image";
+import { BiCaretDown } from "react-icons/bi";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlLocationPin } from "react-icons/sl";
 
 const Header = () => {
   return (
     <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
       <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
-        <Image className="w-28 object-cover" src={logo} alt="logoImg" />
+        {/* logo */}
+        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]">
+          <Image className="w-28 object-cover mt-1" src={logo} alt="logoImg" />
+        </div>
+        {/* delivery */}
+        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%] hidden xl:inline-flex gap-1">
+          <SlLocationPin />
+          <div className="text-xs">
+            <p>Deliver to</p>
+            <p className="text-white font-bold uppercase">USA</p>
+          </div>
+        </div>
+        {/* searchbar */}
+        <div className="flex-1 h-10 hidden md:inline-flex items-center justify-between relative">
+          <input type="text" placeholder="Search next_amazon_yt products" />
+        </div>
       </div>
     </div>
   );
