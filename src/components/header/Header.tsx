d20426d5bcd5a088a21623ebd5d0c6e6d0aec086
Deepak Meena <deepak07itmeena@gmail.com>
2025-01-05 09:57:19 +0530

    2nd commit

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import logo from "../../images/logo.png";
+import Image from "next/image";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
+      <div className="h-full w-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
+        <Image className="w-28 object-cover" src={logo} alt="logoImg" />
+      </div>
+    </div>
+  );
 };
 export default Header;
