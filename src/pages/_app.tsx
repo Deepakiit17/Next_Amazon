d1dbffb2a48cb3f19f598ebda99d72f59970356e
Deepak Meena <deepak07itmeena@gmail.com>
2025-01-06 18:24:42 +0530

    8th commit Product component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,11 @@
 import RootLayout from "@/components/RootLayout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="font-bodyFont">
+    <div className="font-bodyFont bg-gray-300">
       <RootLayout>
         <Component {...pageProps} />
       </RootLayout>
